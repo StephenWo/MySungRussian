c9f548ef65df18f828267c905cabe11a2f1597ba
chengchen-liu90 <chengchen.liu90@gmail.com>
2016-03-07 17:51:34 -0500

    Attempted to make word and IPA alignment, by measuring the width in runtime and determine paddings. It is quite ugly though.

--- app/src/main/java/com/mysungrussian/mysungrussian/MainActivity.java
+++ app/src/main/java/com/mysungrussian/mysungrussian/MainActivity.java
@@ -15,6 +15,9 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class MainActivity extends AppCompatActivity
         implements TranscribeFragment.OnFragmentInteractionListener, SavedFragment.OnFragmentInteractionListener {
     TranscribeFragment transFrag;
@@ -36,7 +39,7 @@ public class MainActivity extends AppCompatActivity
 
 
         // For hiding keyboard when outside text box is clicked
-        setupUI( findViewById(R.id.fragment_container) );
+        setupUI(findViewById(R.id.fragment_container));
     }
 
     // Method to hide keyboard
@@ -107,51 +110,113 @@ public class MainActivity extends AppCompatActivity
         }
 
         EditText input_field = (EditText) findViewById(R.id.input_field);
-        input_field.setText("Здравствуйте тотчас же время работы по созданию интеллектуальных систем видеонаблюдения на избирательных линий розлива растительного происхождения товаров для личного использования");
-        String input_words = input_field.getText().toString();
+        //input_field.setText("Здравствуйте тотчас же время работы по созданию интеллектуальных систем видеонаблюдения на избирательных линий розлива растительного происхождения товаров для личного использования");
+        String input = input_field.getText().toString();
+        input_field.setVisibility(View.INVISIBLE);
         input_field.clearFocus();
+        int field_width = input_field.getWidth();
+        Log.d("Che", "Field width is "+field_width);
 
-        Log.d("Che", "transcribing, the input is: " +input_words);
+        Log.d("Che", "transcribing, the input is: " + input);
 
-        if (!input_words.isEmpty()) {
-            // Feed into RuleEngine, the result is a string of IPA
-            String output_ipas = RuleEngine.Transcribe(input_words);
-            Log.d("Che", "transcribing, the output is: " + output_ipas);
-
-            // Tokenize the input and output string, pad spaces to make them equal length
-
-            // Put output
+        if (!input.isEmpty()) {
+            // Get our output_field~
             EditText output_field = (EditText) findViewById(R.id.output_field2);
+            output_field.setVisibility(View.INVISIBLE);
+
+            // The formatted strings from this trunk of code!
+            String formatted_input = "";
+            String formatted_output = "";
+            int line_width = 0;
+
+
+            //Split into sentences before feeding into RuleEngine.Transcribe()
+            String[] input_sentences = input.split("[,.!?]");
+            for (int counter = 0; counter<input_sentences.length; counter ++) {
+                // Feed into RuleEngine, call Transcribe()
+                // (the result is a string of IPAs)
+                String input_sentence = input_sentences[counter];
+                input_sentence = input_sentence.replaceAll("\n", "");   //Strip newlines
+                input_sentence = input_sentence.replaceAll(" +", " ");   //Strip spaces if >1 space
+                Log.d("Che", "transcribing sentence "+input_sentence);
+
+                String output_ipas = RuleEngine.Transcribe(input_sentence);
+                output_ipas = output_ipas.substring(1);
+                Log.d("Che", "transcribed sentence #" + counter + ", the ipa is:" + output_ipas);
+
+                // Tokenize the input and output string with space token
+                String[] input_words = input_sentence.split(" ");
+                String[] output_words = output_ipas.split(" ");
+
+                for (int i = 0; i<input_words.length; i++) {
+                    String temp_in = input_words[i];
+                    String temp_out = output_words[i];
+                    Log.d("Che", "input word: "+ temp_in+", output ipa: " + temp_out);
+
+                    // Put strings into EditText for measuring
+                    input_field.setText(temp_in);
+                    output_field.setText(temp_out);
+
+                    input_field.measure(0, 0);
+                    int width = input_field.getMeasuredWidth(); //BUG! I often get wrong width!
+                    output_field.measure(0, 0);
+                    int width2 = output_field.getMeasuredWidth();
+                    Log.d("Che", "input +width: "+ width+", output +width2: " + width2);
+
+
+                    // Handle breaking into new line
+                    /*line_width += (width2>width) ? width2:width;
+                    Log.d("Che", "line_width = "+line_width);
+                    if (line_width>field_width){
+                        Log.d("Che", "oh " + formatted_input);
+                        temp_in = "\n" + temp_in;
+                        temp_out = "\n" + temp_out;
+                        line_width = 0;
+                    }*/
+
+                    // Handle end of sentence
+                    if (counter==input_words.length-1) {
+                        temp_in += ".";
+                        temp_out += ".";
+                    }
+
+                    // Handle different length: pad space
+                    while ( (width2-width) > 4 ){
+                        // IPA is longer than word
+                        temp_in += " ";
+                        width += 7;
+                    }
+                    while ( (width-width2) > 4 ){
+                        // word is longer than IPA
+                        temp_out += " ";
+                        width2 += 7;
+                    }
+
+                    // Always add a space at the end
+                    temp_in += " ";
+                    temp_out += " ";
+
+                    // Always append words onto formatted strings
+                    formatted_input += temp_in;
+                    formatted_output += temp_out;
+
+                    //formatted_input = formatted_input.substring(0,formatted_input.length()-2) + ". ";
+                    //formatted_output = formatted_output.substring(0,formatted_input.length()-2) + ". ";
+                }
+            }
+
+            // Put formatted input output into EditText
+            input_field.setVisibility(View.VISIBLE);
+            input_field.setText(formatted_input);
             output_field.setVisibility(View.VISIBLE);
-            output_field.clearFocus();
-            output_field.setText(output_ipas);
+            output_field.setText(formatted_output);
 
-            // Bring the input_field to front (for now, only allow editting of this)
+            // Bring the input_field to front (for now, only allow editing of words, not IPAs)
             input_field.bringToFront();
-            input_field.clearFocus();
+
         }
 
 
-    }
-    /*public void onClickInputField (View v) {
-        //hideSoftKeyboard();
-        // Check if no view has focus:
-        View view = this.getCurrentFocus();
-        if (view != null) {
-            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-        }
-
-        EditText input_field = (EditText) findViewById(R.id.input_field);
-        input_field.clearFocus();
-        input_field.hasFocus();
-    }*/
-
-    private void hideSoftKeyboard() {
-        if (getCurrentFocus()!=null) {
-            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
-            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
-        }
     }
 
 
