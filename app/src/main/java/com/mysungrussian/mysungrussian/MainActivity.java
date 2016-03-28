a8e12af7b72e756d797066572f28692fcb31bb75
chengchen-liu90 <chengchen.liu90@gmail.com>
2016-03-27 20:53:02 -0400

    focus/remove focus, copy button

--- app/src/main/java/com/mysungrussian/mysungrussian/MainActivity.java
+++ app/src/main/java/com/mysungrussian/mysungrussian/MainActivity.java
@@ -1,6 +1,5 @@
 package com.mysungrussian.mysungrussian;
 
-import android.app.ActionBar;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.FragmentManager;
@@ -14,8 +13,8 @@ import android.net.Uri;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.ClipboardManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -25,9 +24,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.FileOutputStream;
@@ -95,7 +92,8 @@ public class MainActivity extends AppCompatActivity
         PorterDuff.Mode mode = PorterDuff.Mode.SRC_ATOP;
         // add your drawable resources you wish to tint to the drawables array...
         int drawables[] = { R.drawable.ic_clear_black_24dp, R.drawable.technology, R.drawable.ic_keyboard_arrow_up_white_24dp,
-                            R.drawable.ic_keyboard_arrow_down_white_24dp, R.drawable.ic_content_copy_white_24dp};
+                            R.drawable.ic_keyboard_arrow_down_white_24dp, R.drawable.ic_content_copy_white_24dp,
+                            R.drawable.ic_swap_vert_black_24dp};
         for (int id : drawables) {
             Drawable icon = getResources().getDrawable(id);
             icon.setColorFilter(tint,mode);
@@ -181,7 +179,6 @@ public class MainActivity extends AppCompatActivity
 
     // Buttons in TranscribeFragment
     public void onClickTranscribe (View v){
-        //hideSoftKeyboard();
         // Check if no view has focus:
         View view = this.getCurrentFocus();
         if (view != null) {
@@ -194,7 +191,7 @@ public class MainActivity extends AppCompatActivity
 
         //input_field.setText("Здравствуйте тотчас же время работы по созданию интеллектуальных систем видеонаблюдения на избирательных линий розлива растительного происхождения товаров для личного использования");
         String input = input_field.getText().toString();
-        input_field.setVisibility(View.INVISIBLE);
+        //input_field.setVisibility(View.INVISIBLE);
         input_field.clearFocus();
         int field_width = input_field.getWidth();
         Log.d("Che", "Field width is " + field_width);
@@ -202,18 +199,23 @@ public class MainActivity extends AppCompatActivity
         Log.d("Che", "transcribing, the input is: " + input);
 
         if (!input.isEmpty()) {
-            // Get our output_field~
-            output_field.setVisibility(View.INVISIBLE);
+            //output_field.setVisibility(View.INVISIBLE);
             output_field.setFocusable(false);
 
             // The formatted strings from this trunk of code!
             String formatted_input = "";
             String formatted_output = "";
-            int line_width = 0;
+            int line_width = findViewById(R.id.scrollView).getWidth();
+            Log.d("Che", "line width is "+line_width);
 
 
             //Split into sentences before feeding into RuleEngine.Transcribe() TODO: put this into RuleEngine
             String[] input_sentences = input.split("[,.!?]");
+            // TODO: keep the 标点符号 for handling end of sentence later. Right now this code just strip any non-word character.
+
+            int sum_width = 0;
+            int sum_width2 = 0;
+
             for (int counter = 0; counter<input_sentences.length; counter ++) {
                 // Feed into RuleEngine, call Transcribe()
                 // (the result is a string of IPAs)
@@ -228,30 +230,24 @@ public class MainActivity extends AppCompatActivity
                 Log.d("Che", "transcribing sentence "+input_sentence);
 
                 String output_ipas = RuleEngine.Transcribe(input_sentence);
-                output_ipas = output_ipas.substring(1);
                 Log.d("Che", "transcribed sentence #" + counter + ", the ipa is:" + output_ipas);
 
                 // Tokenize the input and output string with space token
                 String[] input_words = input_sentence.split(" ");
                 String[] output_words = output_ipas.split(" ");
 
-
                 // Put word-pairs in one by one?
                 for (int i = 0; i<input_words.length; i++) {
                     String temp_in = input_words[i];
                     String temp_out = output_words[i];
-                    Log.d("Che", "input word: "+ temp_in+", output ipa: " + temp_out);
+                    int width = -1;
+                    int width2 = -1;
+                    Log.d("Che", "input word: " + temp_in + ", output ipa: " + temp_out);
 
                     // Put strings into EditText for measuring
-                    input_field.setText(temp_in);
-                    output_field.setText(temp_out);
-
-                    input_field.measure(0, 0);
-                    int width = input_field.getMeasuredWidth(); //BUG! I often get wrong width!
-                    output_field.measure(0, 0);
-                    int width2 = output_field.getMeasuredWidth();
-                    Log.d("Che", "input +width: "+ width+", output +width2: " + width2);
-
+                    width = getWidth(input_field, temp_in);
+                    width2 = getWidth(output_field, temp_out);
+                    Log.d("Che", "Before: input +width: " + width + ", output +width2: " + width2);
 
                     // Handle breaking into new line
                     /*line_width += (width2>width) ? width2:width;
@@ -264,38 +260,51 @@ public class MainActivity extends AppCompatActivity
                     }*/
 
                     // TODO: Handle end of sentence, add back the sign
-                    if (counter==input_words.length-1) {
+                    if (counter == input_words.length - 1) {
                         //temp_in += ".";
                         //temp_out += ".";
                     }
 
-                    // Handle different length: pad space
-                    while ( (width2-width) > 4 ){
-                        // IPA is longer than word
-                        temp_in += " ";
-                        width += 7;
-                    }
-                    while ( (width-width2) > 4 ){
-                        // word is longer than IPA
-                        temp_out += " ";
-                        width2 += 7;
+                    // Determine if there should be a newline before this word
+                    int height = getHeight(input_field, formatted_input+temp_in+" ");
+                    int height2 = getHeight(output_field, formatted_output+temp_out+" ");
+                    Log.d("Che", "Before: input height: " + height + ", output height: " + height2);
+
+                    if (height-height2>20) {
+                        Log.d("Che", "\n\nadding new line\n\n");
+                        formatted_input += "\n";
+                        formatted_output += "\n";
                     }
 
                     // Always add a space at the end
                     temp_in += " ";
                     temp_out += " ";
 
+                    // Handle different length: pad space
+                    while ((width2 - width) > 3) {
+                        temp_in += " ";
+                        width = getWidth(input_field, temp_in);
+                        Log.d("Che", "pad to input, width" + width);
+                    }
+                    while ((width - width2) > 3) {
+                        // word is longer than IPA
+                        temp_out += " ";
+                        width2 = getWidth(output_field, temp_out);
+                        Log.d("Che", "pad to output, temp_out is " + temp_out + ", width2 " + width2);
+                    }
+
+                    Log.d("Che", "After: input +width: " + width + ", output +width2: " + width2);
+
+                    sum_width += width;
+                    sum_width2 += width2;
+                    Log.d("Che", "After: input sum width: " + sum_width + ", output sum width: " + sum_width2);
+
                     // Always append words onto formatted strings
                     formatted_input += temp_in;
                     formatted_output += temp_out;
                 }
-            }
 
-            // Put formatted input output into EditText
-            /*input_field.setVisibility(View.VISIBLE);
-            input_field.setText(formatted_input);
-            output_field.setVisibility(View.VISIBLE);
-            output_field.setText(formatted_output);*/
+            }
 
             current_formatted_input = formatted_input;
             current_formatted_output = formatted_output;
@@ -305,6 +314,23 @@ public class MainActivity extends AppCompatActivity
         else {
             output_field.setText("");
         }
+    }
+
+    private int getWidth (EditText field, String str){
+        field.setVisibility(View.VISIBLE);
+        field.setText(str + ".");
+        field.measure(0, 0);
+        int width = field.getMeasuredWidth()-1;
+        return width;
+    }
+
+    private int getHeight (EditText field, String str){
+        Log.d("Che", "getHeight, str is "+str);
+        field.setVisibility(View.VISIBLE);
+        field.setText(str);
+        field.measure(0, 0);
+        int height = field.getMeasuredHeight();
+        return height;
     }
 
     public void mySetText (){
@@ -320,10 +346,24 @@ public class MainActivity extends AppCompatActivity
             output_field.setText(current_formatted_output);
 
             // Bring the input_field to front (for now, only allow editing of words, not IPAs)
-            input_field.bringToFront();
+            myFocusField(input_field);
 
+            Log.d("Che", "mySetText, focus requested");
         }
 
+    }
+
+    private void myFocusField (EditText field){
+        field.bringToFront();
+        field.setFocusable(true);
+        field.setFocusableInTouchMode(true);
+        field.setSelection(field.getText().length());
+        field.requestFocus();
+    }
+
+    private void myRemoveFocusField (EditText field){
+        field.setFocusable(false);
+        field.setFocusableInTouchMode(false);
     }
 
     public void clearText(View v) {
@@ -341,20 +381,57 @@ public class MainActivity extends AppCompatActivity
         output_field.setVisibility(View.VISIBLE);
         output_field.setText(current_formatted_output);
 
-        input_field.bringToFront();
+        myFocusField(input_field);
+        myRemoveFocusField(output_field);
     }
 
     public void buttonOnClick (View v){
         switch (v.getId()) {
             case R.id.btn:
                 onClickTranscribe(v);
-                onClickTranscribe(v);   //Somehow calling this twice sets the format correctly.. (:3...
+                //onClickTranscribe(v);   //Somehow calling this twice sets the format correctly.. (:3...
                 break;
             case R.id.btn_clear:
                 clearText(v);
+                Toast.makeText(getApplicationContext(), "Cleared.", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.imageButton_save_trans:
                 showInputDialog();
+                Toast.makeText(getApplicationContext(), "Saved.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_copy:
+                String input_sentence = current_formatted_input.replaceAll("\n", "");   //Strip newlines
+                input_sentence = input_sentence.replaceAll(" +", " ");   //Strip spaces if >1 space
+
+                String output_sentence = current_formatted_output.replaceAll("\n", "");   //Strip newlines
+                output_sentence = output_sentence.replaceAll(" +", " ");   //Strip spaces if >1 space
+
+                // Get clipboard
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+                clipboard.setText(input_sentence + "\n" + output_sentence);
+
+                Toast.makeText(getApplicationContext(), "Copied both input and output to clipboard.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_swap_vert:
+                View currentFocus = getCurrentFocus();
+                EditText out = (EditText) findViewById(R.id.output_field2);
+                EditText in = (EditText) findViewById(R.id.input_field);
+                switch (currentFocus.getId()) {
+                    case R.id.input_field:
+                        myFocusField(out);
+                        myRemoveFocusField(in);
+                        Toast.makeText(getApplicationContext(), "Swap to edit output.", Toast.LENGTH_SHORT).show();
+
+                        break;
+                    case R.id.output_field2:
+                        myFocusField(in);
+                        myRemoveFocusField(out);
+                        Toast.makeText(getApplicationContext(), "Swap to edit input.", Toast.LENGTH_SHORT).show();
+
+                        break;
+                    default:
+                        Toast.makeText(getApplicationContext(), "Please focus somewhere first.", Toast.LENGTH_SHORT).show();
+                }
                 break;
             case R.id.btn_record:
                 learnFrag.onClickRecord(v);
